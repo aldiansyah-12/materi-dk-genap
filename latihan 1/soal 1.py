fd27ad891bbe35639cd13e101a2f848b382cfbeb
galdi2045@gmail.com <theprogfanteh@gmail.com>
2025-01-14 07:47:13 +0700

    menambahkan pesan baru

---
+++ latihan 1/soal 1.py
@@ -0,0 +1,8 @@
+#program untuk membuat kelipatqan empat
+
+bilangan=int(input("masukan bilangan :"))
+
+if bilangan %4==0:
+    print(bilangan, "kelipatan 4")
+else :
+    print(bilangan,"bukan kelipatan 4")
---
+++ latihan 1/soal 2.py
@@ -0,0 +1,1 @@
+#program menentukan harga belanja dari diskon 5%
